34413e830bd5392a0c7210ea6f0a566239a184f6
danwaterfield <36337888+danwaterfield@users.noreply.github.com>
2019-03-15 13:36:37 +0000

    full program

--- quickweather.py
+++ quickweather.py
@@ -1,7 +1,7 @@
 #! python3
 # quickweather.py - Prints the weather for a location from the command line.
 
-import json, requests, sys
+import json, requests, sys, bs4
 
 # figures out location from the command line inputs
 
@@ -11,6 +11,28 @@ if len(sys.argv) < 2:
 
 location = ' '.join(sys.argv[1:])
 
-# to do download the json data from openweathermap's API
+# downloads JSON data from openweather's API. Workaround due to the site requiring an API key. Currently waiting for the site to auth my key...
+
+APPID = ''
+
+url = 'api.openweathermap.org/data/2.5/weather?q=%s'+ location + '&APPID=' + APPID
+response = requests.get(url)
+response.raise_for_status()
 
 # load jason data into a python variable.
+weatherData = json.loads(response.text)
+
+pprint.pprint(weatherData)
+
+# prints weather data
+
+w = weatherData['list']
+print('Current weather in %s is: ' % (location))
+print(w[0]['weather'][0]['main'], '-', w[0]['weather'][0]['description'])
+print()
+print('Tomorrow: ')
+print(w[1]['weather'][0]['main'], '-', w[1]['weather'][0]['description'])
+print()
+print('Day after tomorrow: ')
+print(w[2]['weather'][0]['main'], '-', w[2]['weather'][0]['description'])
+
