38b0a6969b2ca722906ae8fe8ec51d0d1a4dd782
danwaterfield <36337888+danwaterfield@users.noreply.github.com>
2019-03-15 19:26:52 +0000

    Update downloadXKCD.py

--- downloadXKCD.py
+++ downloadXKCD.py
@@ -2,3 +2,13 @@
 #downloadxkcd.py - downloads every single xkcd comic. Yeah i don't know why either...
 
 url = 'http://xkcd.com' #this is the starting url
+os.makedirs('xkcd', exist_ok=True) #stores the comics in ./xkcd
+
+while not url.endswith('#'):
+	#to do: download the page
+	#find the url of the comic image
+	#download the image
+	#save image to xkcd
+	#get the prev button's url	
+
+
